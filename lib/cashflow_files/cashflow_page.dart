f5b88581eb6b57af9516962e65d11c67aeb54a99
Husk09 <parmar.kush2007@gmail.com>
2024-08-30 03:09:08 -0400

    Continued work on cashflow page, graph for monthly has been created and implemented

--- lib/cashflow_files/cashflow_page.dart
+++ lib/cashflow_files/cashflow_page.dart
@@ -1,9 +1,12 @@
+import 'dart:math';
+
+import 'package:econowise/budget_files/budget.dart';
+import 'package:econowise/transaction_files/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
-import 'package:econowise/transaction_files/transaction.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:econowise/save_data.dart';
-import 'package:econowise/budget_files/budget.dart';
 
 class CashflowPage extends StatefulWidget {
   const CashflowPage({super.key});
@@ -46,11 +49,70 @@ class _CashflowPageState extends State<CashflowPage> {
                 getTotalExpenseChange(),
                 getTotalCashflowChange(),
                 isMonthlyView,
-                toggleView)
+                toggleView),
+            Consumer<SaveData>(
+              builder: (context, saveData, _) {
+                final transactions = saveData.transactions;
+                final chartSeries =
+                    getChartSeriesData().cast<CartesianSeries>();
+                final axisRange = calculateYAxisRange(transactions);
+
+                return SfCartesianChart(
+                  primaryXAxis: CategoryAxis(),
+                  primaryYAxis: NumericAxis(
+                    minimum: axisRange.$1,
+                    maximum: axisRange.$2,
+                    interval: axisRange.$3,
+                  ),
+                  series: chartSeries,
+                  onSelectionChanged: (SelectionArgs args) {
+                    if (args.pointIndex != null &&
+                        args.seriesIndex != null &&
+                        args.seriesIndex! < chartSeries.length) {
+                      final series =
+                          chartSeries[args.seriesIndex!] as StackedColumnSeries;
+                      final budgetName = series.name ?? 'Other';
+
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Budget: $budgetName')),
+                      );
+                    }
+                  },
+                );
+              },
+            ),
           ],
         ),
       ),
     );
+  }
+
+  (double, double, double) calculateYAxisRange(List<Transaction> transactions) {
+    // Calculate total income and expenses for the active month and year
+    double totalIncome = getTotalIncome(activeMonth, activeYear).toDouble();
+    double totalExpenses = getTotalExpenses(activeMonth, activeYear).toDouble();
+
+    // Determine the range based on the higher of total income or total expenses
+    double range = max(totalIncome, totalExpenses);
+
+    // Handle the case where there are no transactions
+    if (range == 0) {
+      range = 100;
+    }
+
+    // Round up the range to the nearest 500 or multiple of 1000
+    if (range <= 500) {
+      range = 500;
+    } else {
+      int powerOfTen = (range.abs().toString().length - 1).clamp(2, 5);
+      double intervalBase = pow(10, powerOfTen).toDouble();
+      range = (range / intervalBase).ceil() * intervalBase;
+    }
+
+    // Calculate interval
+    double interval = 100 + ((range - 100) / 500).floor() * 100;
+
+    return (-range, range, interval);
   }
 
   void nextMonth() {
@@ -122,10 +184,6 @@ class _CashflowPageState extends State<CashflowPage> {
       num currentYearIncome = getTotalIncomeForYear(activeYear);
       num previousYearIncome = getTotalIncomeForYear(previousYear);
 
-      if (previousYearIncome == 0) {
-        return 0;
-      }
-
       return ((currentYearIncome - previousYearIncome) / previousYearIncome) *
           100;
     }
@@ -142,10 +200,6 @@ class _CashflowPageState extends State<CashflowPage> {
       num currentYearExpenses = getTotalExpensesForYear(activeYear);
       num previousYearExpenses = getTotalExpensesForYear(previousYear);
 
-      if (previousYearExpenses == 0) {
-        return 0;
-      }
-
       return ((currentYearExpenses - previousYearExpenses) /
               previousYearExpenses) *
           100;
@@ -159,10 +213,6 @@ class _CashflowPageState extends State<CashflowPage> {
       num previousMonthCashflow = getTotalIncome(activeMonth - 1, activeYear) -
           getTotalExpenses(activeMonth - 1, activeYear);
 
-      if (previousMonthCashflow == 0) {
-        return 0;
-      }
-
       num absoluteChange = currentMonthCashflow - previousMonthCashflow;
       num percentageChange =
           (absoluteChange / previousMonthCashflow.abs()) * 100;
@@ -174,10 +224,6 @@ class _CashflowPageState extends State<CashflowPage> {
           getTotalExpensesForYear(activeYear);
       num previousYearCashflow = getTotalIncomeForYear(previousYear) -
           getTotalExpensesForYear(previousYear);
-
-      if (previousYearCashflow == 0) {
-        return 0;
-      }
 
       num absoluteChange = currentYearCashflow - previousYearCashflow;
       num percentageChange =
@@ -205,6 +251,174 @@ class _CashflowPageState extends State<CashflowPage> {
       }
     }
     return totalExpenses;
+  }
+
+  List<ChartSeries<ChartData, String>> getChartSeriesData() {
+    List<Transaction> transactions = context.read<SaveData>().transactions;
+    List<Budget> budgets = context.read<SaveData>().budgets;
+
+    // 1. Get all weeks of the active month
+    List<DateTimeRange> weeksOfMonth = getWeeksOfMonth(activeYear, activeMonth);
+
+    // 2. Initialize weeklyBudgetExpenses with all weeks and budgets (or 'Other')
+    Map<DateTimeRange, Map<String, num>> weeklyBudgetExpenses = {};
+    for (var weekRange in weeksOfMonth) {
+      weeklyBudgetExpenses[weekRange] = {};
+      if (budgets.isNotEmpty) {
+        for (var budget in budgets) {
+          weeklyBudgetExpenses[weekRange]![budget.goal] = 0;
+        }
+      }
+      // Always initialize 'Other' even if there are other budgets
+      weeklyBudgetExpenses[weekRange]!['Other'] = 0;
+      weeklyBudgetExpenses[weekRange]!['Income'] = 0;
+    }
+
+    // 3. Group transactions by week range and budget/income (update existing values)
+    for (var transaction in transactions) {
+      if (transaction.date != null &&
+          transaction.date!.month == activeMonth &&
+          transaction.date!.year == activeYear) {
+        DateTimeRange? weekRange = weeksOfMonth.firstWhere(
+          (range) =>
+              transaction.date!.isAfter(range.start) &&
+              transaction.date!.isBefore(range.end),
+          orElse: () => weeksOfMonth.last,
+        );
+
+        String categoryName;
+        num transactionValue;
+
+        if (transaction.spent) {
+          categoryName = budgets.any((b) => b.goal == transaction.budget.goal)
+              ? transaction.budget.goal
+              : 'Other';
+          transactionValue = -transaction.amount; // Expenses are negative
+        } else {
+          categoryName = 'Income';
+          transactionValue = transaction.amount; // Income is positive
+        }
+
+        weeklyBudgetExpenses[weekRange]![categoryName] =
+            (weeklyBudgetExpenses[weekRange]![categoryName]! +
+                transactionValue);
+      }
+    }
+
+    // 4. Calculate min, max, and interval for the y-axis
+    num minValue = 0;
+    num maxValue = 0;
+    for (var weekExpenses in weeklyBudgetExpenses.values) {
+      num weekTotal = weekExpenses.values.fold(0, (sum, value) => sum + value);
+      minValue = min(minValue, weekTotal);
+      maxValue = max(maxValue, weekTotal);
+    }
+
+    // Adjust min/max to ensure some padding around the data
+    minValue = minValue * 1.1; // 10% padding below
+    maxValue = maxValue * 1.1; // 10% padding above
+
+    // Calculate a suitable interval
+    num interval =
+        (maxValue - minValue) / 5; // Divide the range into 5 intervals
+    interval = (interval / 100).ceil() * 100; // Round up to the nearest 100
+
+    // 5. Create ChartSeries
+    List<ChartSeries<ChartData, String>> chartSeries = [];
+
+    // Handle the case where there are no budgets at all OR there are 'Other' expenses
+    if ((budgets.isEmpty && weeklyBudgetExpenses.isNotEmpty) ||
+        weeklyBudgetExpenses.values
+            .any((expenses) => expenses.containsKey('Other'))) {
+      chartSeries.add(createSeriesForCategory(
+        'Other',
+        Color.fromARGB(255, 128, 147, 241),
+        weeksOfMonth,
+        weeklyBudgetExpenses,
+      ));
+    }
+
+    // Create series for each budget
+    for (var budget in budgets) {
+      chartSeries.add(
+          createSeriesForBudget(budget, weeksOfMonth, weeklyBudgetExpenses));
+    }
+
+    // Always include the 'Income' series if there's any income
+    if (weeklyBudgetExpenses.values
+        .any((expenses) => expenses.containsKey('Income'))) {
+      chartSeries.add(createSeriesForCategory(
+        'Income',
+        Colors.green,
+        weeksOfMonth,
+        weeklyBudgetExpenses,
+      ));
+    }
+
+    return chartSeries;
+  }
+
+// Generalized function to create series for both budgets and income
+  ChartSeries<ChartData, String> createSeriesForCategory(
+      String categoryName,
+      Color color,
+      List<DateTimeRange> weeksOfMonth,
+      Map<DateTimeRange, Map<String, num>> weeklyBudgetExpenses) {
+    List<ChartData> chartData = [];
+    for (var weekRange in weeksOfMonth) {
+      num valueForCategory =
+          weeklyBudgetExpenses[weekRange]![categoryName] ?? 0;
+      String weekLabel =
+          '${DateFormat('MMM d').format(weekRange.start)} - ${DateFormat('d').format(weekRange.end)}';
+      chartData.add(ChartData(weekLabel, valueForCategory));
+    }
+
+    return StackedColumnSeries<ChartData, String>(
+      dataSource: chartData,
+      xValueMapper: (ChartData data, _) => data.x,
+      yValueMapper: (ChartData data, _) => data.y,
+      name: categoryName,
+      color: color,
+    );
+  }
+
+  ChartSeries<ChartData, String> createSeriesForBudget(
+      Budget budget,
+      List<DateTimeRange> weeksOfMonth,
+      Map<DateTimeRange, Map<String, num>> weeklyBudgetExpenses) {
+    List<ChartData> chartData = [];
+    for (var weekRange in weeksOfMonth) {
+      num expenseForBudget = weeklyBudgetExpenses[weekRange]![budget.goal] ?? 0;
+      String weekLabel =
+          '${DateFormat('MMM d').format(weekRange.start)} - ${DateFormat('d').format(weekRange.end)}';
+      chartData.add(ChartData(weekLabel, expenseForBudget));
+    }
+
+    return StackedColumnSeries<ChartData, String>(
+      dataSource: chartData,
+      xValueMapper: (ChartData data, _) => data.x,
+      yValueMapper: (ChartData data, _) => data.y,
+      name: budget.goal,
+      color: budget.color,
+    );
+  }
+
+  List<DateTimeRange> getWeeksOfMonth(int year, int month) {
+    List<DateTimeRange> weeks = [];
+    DateTime firstDayOfMonth = DateTime(year, month, 1);
+    DateTime lastDayOfMonth = DateTime(year, month + 1, 0);
+
+    DateTime currentWeekStart = firstDayOfMonth;
+    while (currentWeekStart.isBefore(lastDayOfMonth)) {
+      DateTime currentWeekEnd = currentWeekStart.add(const Duration(days: 6));
+      if (currentWeekEnd.isAfter(lastDayOfMonth)) {
+        currentWeekEnd = lastDayOfMonth;
+      }
+      weeks.add(DateTimeRange(start: currentWeekStart, end: currentWeekEnd));
+      currentWeekStart = currentWeekEnd.add(const Duration(days: 1));
+    }
+
+    return weeks;
   }
 }
 
@@ -350,4 +564,11 @@ Widget financialSummaryRow({
       ],
     ),
   );
+}
+
+class ChartData {
+  final String x;
+  final num y;
+
+  ChartData(this.x, this.y);
 }

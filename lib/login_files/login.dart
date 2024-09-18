3674dd397c1a68facc9e0be3f9944fde805256aa
Husk09 <parmar.kush2007@gmail.com>
2024-09-17 22:23:44 -0400

    Set Up Firebase Connection and Login System

---
+++ lib/login_files/login.dart
@@ -0,0 +1,67 @@
+import 'package:econowise/navigation_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'package:econowise/firebase_options.dart'; // Import your Firebase configuration
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<UserCredential?> signInWithGoogle() async {
+    try {
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ); // Initialize Firebase
+
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) return null;
+
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      return await _auth.signInWithCredential(credential);
+    } catch (e) {
+      // Handle sign-in errors here
+      print("Error signing in with Google: $e");
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            UserCredential? userCredential = await signInWithGoogle();
+            if (userCredential != null) {
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder: (context) => MenuSelecter(index: 1)));
+              print("Signed in");
+            } else {
+              print("Didn't work");
+            }
+          },
+          child: const Text('Sign in with Google'),
+        ),
+      ),
+    );
+  }
+}
